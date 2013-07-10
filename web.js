f17f01d4c9ac2ec2520eeecff7cb67c746394552
Wenbo Chang <enquea@gmail.com>
2013-07-10 06:04:23 +0000

    Edit web.js to display index

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var read = fs.readFileSync('index.html');  
+  var buff = new Buffer(read, 'utf-8');
+  var msg = buff.toString();j 
+
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
